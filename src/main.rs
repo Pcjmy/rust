73cdd7ca94105d7868191ef6fcceed63b04ab77a
Pcjmy <1716045191@qq.com>
2024-05-02 17:03:02 +0800

    feat: mut

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // 创建变量：let 关键字
 // 变量默认是不可变的
+// 可变变量：变量名称前加 mut
 
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is {}", x);
 
-    // cannot assign twice to immutable variable
-    // x = 6;
-    // println!("The value of x is {}", x);
+    x = 6;
+    println!("The value of x is {}", x);
 }
