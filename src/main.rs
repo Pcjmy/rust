dde3a5bffd529b5af906f5c66203b757e40fcb5a
Pcjmy <1716045191@qq.com>
2024-05-02 17:01:21 +0800

    feat: 变量默认不可变

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 // 创建变量：let 关键字
+// 变量默认是不可变的
 
 fn main() {
     let x = 5;
     println!("The value of x is {}", x);
+
+    // cannot assign twice to immutable variable
+    // x = 6;
+    // println!("The value of x is {}", x);
 }
