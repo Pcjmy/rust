5fe776a72942fce6e5eeedc8a643983b20aaf503
Pcjmy <1716045191@qq.com>
2024-05-04 15:11:56 +0800

    feat: 数组越界

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     let myarray: [u32; 5] = [1, 2, 3, 4, 5];
 
     println!("myarray[1] = {}", myarray[1]);
+
+    let index = "5".parse::<usize>().unwrap();
+
+    println!("myarray[5] = {}", myarray[index]);
 }
