43fcbdc50943638b7fb3e69bc6750b4476d6cb40
Pcjmy <1716045191@qq.com>
2024-05-04 12:18:17 +0800

    feat: 元组

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-// 创建变量：let 关键字
-// 变量默认是不可变的
-// 可变变量：变量名称前加 mut
-// 常量：const 关键字
-// Shadowing: 隐藏
-
 fn main() {
-    let a: u32 = "4294967295".parse::<u32>().unwrap();
-    let b: u32 = 1;
+    let a = 10;
+    let b = 'A';
 
-    let (mul, is_overflow) = a.overflowing_mul(b);
-    println!("mul = {:?}, is_overflow={:?}", mul, is_overflow);
+    let mytuple = (a, b);
+
+    let (c, d) = mytuple;
+    println!("c={} d={}", c, d);
 }
