a4c3dd6b08ac2dea5103930edd2abf2b9e16e23c
Pcjmy <1716045191@qq.com>
2024-05-04 15:14:51 +0800

    feat: 数组初始化

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ fn main() {
 
     println!("myarray[1] = {}", myarray[1]);
 
-    let index = "5".parse::<usize>().unwrap();
+    // let index = "5".parse::<usize>().unwrap();
 
-    println!("myarray[5] = {}", myarray[index]);
+    // println!("myarray[5] = {}", myarray[index]);
+
+    let mybuffer: [u32; 32 * 1024] = [0; 32 * 1024];
+    println!("mybuffer[1024] = {}", mybuffer[1024]);
 }
