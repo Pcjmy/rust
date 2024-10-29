cbce3367988129cd5eb1f53475733b45752d23d1
Pcjmy <1716045191@qq.com>
2024-10-29 08:50:44 +0800

    feat: 派生属性

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
-// 元组结构
-struct Pair(i32, f32);
-
 // 标准的C结构
+#[derive(Debug)] // 派生属性
 struct Person {
     name: String,
     age: u32,
 }
 
-// 单元结构（无字段，通常在泛型里使用较多）
-struct Unit;
-
 fn main() {
-    let pair = Pair(10, 7.3);
-    println!("{}", pair.0);
-
     let jack = Person {
         name: String::from("jack"),
         age: 6,
     };
-    println!("name={} age={}", jack.name, jack.age);
-
-    let unit = Unit;
+    println!("{:?}", jack);
 }
