9d93a85c20858898b6093e49532ba6e5cdca4b02
Pcjmy <1716045191@qq.com>
2024-10-29 08:26:23 +0800

    feat: 切片类型

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 fn main() {
-    let myarray: [u32; 5] = [1, 2, 3, 4, 5];
-
-    println!("myarray[1] = {}", myarray[1]);
-
-    // let index = "5".parse::<usize>().unwrap();
-
-    // println!("myarray[5] = {}", myarray[index]);
-
-    let mut mybuffer: [u32; 32 * 1024] = [0; 32 * 1024];
-    println!("mybuffer[1024] = {}", mybuffer[1024]);
-
-    mybuffer[1024] = 13;
-
-    println!("mybuffer[1024] = {}", mybuffer[1024]);
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    let slice = &arr[0..3];
+    println!("slice[0]={}, len={}", slice[0], slice.len()); 
 }
