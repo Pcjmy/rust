38616a19d91c02e3c7c6843980b8e8075a4827b3
Pcjmy <1716045191@qq.com>
2024-10-29 08:34:33 +0800

    feat: 可变引用

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
     let slice = &arr[0..3];
-    println!("slice[0]={}, len={}", slice[0], slice.len()); 
+    println!("slice[0]={}, len={}", slice[0], slice.len());
+
+    let slice2 = &arr[3..5];
+    println!("slice2[0]={}, len={}", slice2[0], slice2.len());
+
+    let mut slice3 = &mut arr[..];
+    slice3[0] = 6;
+    println!("arr[0]={}", arr[0]);
 }
