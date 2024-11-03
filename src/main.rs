a119a4a6748ac0e7cfacc1d1dd569177072325ae
Pcjmy <1716045191@qq.com>
2024-11-04 07:47:28 +0800

    feat: 闭包

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::thread;
+
 fn main() {
-    let times3 = |n: u32| -> u32 { n * 3 };
-    println!("{}", times3(10));
+    // move: 将环境中的值移到闭包内部
+    // 使用场景-多线程：从主线程移动值到子线程
+
+    let hello_message = "Hello World!";
+
+    thread::spawn(move || {
+        println!("{}", hello_message);
+    }).join();
 }
