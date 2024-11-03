ce65ad4ca2779cac1c42fb781828dafd6fcbc9c0
Pcjmy <1716045191@qq.com>
2024-11-03 18:07:59 +0800

    feat: for range

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
 fn main() {
-    let mut n = 1;
+    for i in 0..5 {
+        println!("{}", i);
+    }
 
-    while n < 101 {
-        if n % 15 == 0 {
-            println!("fizzbuzz");
-        } else if n % 3 == 0 {
-            println!("fizz");
-        } else if n % 5 == 0 {
-            println!("buzz");
-        } else {
-            println!("{}", n);
-        }
-        n += 1;
+    for i in 0..=5 {
+        println!("{}", i);
     }
 }
