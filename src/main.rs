3460cd9c1cff453f38408074bd51f1df7ee11ef2
Pcjmy <1716045191@qq.com>
2024-11-03 18:55:49 +0800

    feat: 遍历数组

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let myarray = ["a", "b", "c"];
+    let mut myarray = [1, 2, 3];
+
+    for i in myarray.iter_mut() {
+        *i *= 2;
+    }
 
     for i in myarray.iter() {
-        println!("{:?}", i);
+        println!("{}", i);
     }
 }
