0e048ffdce4ecff9062bc6a896435aaefa6e4311
Pcjmy <1716045191@qq.com>
2024-11-04 07:35:40 +0800

    feat: 构造方法

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-fn fibonacci(n: u64) -> u64 {
-    if n < 2 {
-        return n;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+impl Point {
+    // 构造方法
+    fn new(x: u64, y: u64) -> Point {
+        Point { x, y }
     }
 }
 
 fn main() {
-    println!("{:?}", fibonacci(10));
+    let p = Point::new(10, 20);
+    println!("{:?}", p);
 }
