0bdbd79f795810fa226ea5f7a28a39a27bfd2d7b
Pcjmy <1716045191@qq.com>
2024-11-04 07:41:37 +0800

    feat: 匿名函数

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,4 @@
-#[derive(Debug)]
-struct Point {
-    x: u64,
-    y: u64,
-}
-
-impl Point {
-    // 构造方法
-    fn new(x: u64, y: u64) -> Point {
-        Point { x, y }
-    }
-
-    fn get_x(&self) -> u64 {
-        self.x
-    }
-
-    fn set_x(&mut self, x: u64) {
-        self.x = x;
-    }
-}
-
 fn main() {
-    let mut p = Point::new(10, 20);
-    println!("{:?}", p);
-    println!("{:?}", p.get_x());
-
-    p.set_x(30);
-    println!("{:?}", p);
+    let times3 = |n: u32| -> u32 { n * 3 };
+    println!("{}", times3(10));
 }
