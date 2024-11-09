3068da52d9eed23a7cb3d8d97d4eeea00098ac09
Pcjmy <1716045191@qq.com>
2024-11-09 19:32:48 +0800

    feat: 发散函数

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-type Method = fn(u32, u32) -> u32;
-
-fn calc(method: &str) -> Method {
-    match method {
-        "add" => add,
-        "sub" => sub,
-        _ => unimplemented!(),
-    }
-}
-
-fn add(a: u32, b: u32) -> u32 {
-    a + b
-}
-
-fn sub(a: u32, b: u32) -> u32 {
-    a - b
+fn foo() -> ! {
+    panic!("This call never returns.");
 }
 
 fn main() {
-    println!("{}", calc("add")(10, 20));
-    println!("{}", calc("sub")(20, 10));
+    let a = if true {
+        10
+    } else {
+        foo()
+    };
+    println!("{}", a);
 }
