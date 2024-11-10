2bd81deff48af629b0f798712a0ee69eb2c75fed
Pcjmy <1716045191@qq.com>
2024-11-10 19:19:46 +0800

    feat: crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod mod1 {
     pub const MESSAGE: &str = "Hello World!";
 
+    pub(crate) enum CrateEnum {
+        Item = 4
+    }
+
     pub mod mod2 {
         pub const MESSAGE: &str = "Hello World!";
     }
@@ -8,4 +12,5 @@ mod mod1 {
 
 fn main() {
     println!("{}", mod1::mod2::MESSAGE);
+    println!("{}", mod1::CrateEnum::Item as u32);
 }
