19284fa0c5328ffa17f49a13d63ea412ba7b4140
Pcjmy <1716045191@qq.com>
2024-11-10 19:24:22 +0800

    feat: super

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod mod1 {
     pub const MESSAGE: &str = "Hello World!";
+    pub(self) const NUMBER: u32 = 16;
 
     pub(crate) enum CrateEnum {
         Item = 4
@@ -7,10 +8,15 @@ mod mod1 {
 
     pub mod mod2 {
         pub const MESSAGE: &str = "Hello World!";
+
+        pub fn say() {
+            println!("{}", super::NUMBER);
+        }
     }
 }
 
 fn main() {
     println!("{}", mod1::mod2::MESSAGE);
     println!("{}", mod1::CrateEnum::Item as u32);
+    mod1::mod2::say();
 }
