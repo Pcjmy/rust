36b3fb28fe3337990f6e3fb0d2f792f322c35571
Pcjmy <1716045191@qq.com>
2024-11-16 16:13:47 +0800

    feat: 使用super与self简化模块路径

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-use std::fs as stdfs;
+fn function () {
+    println!("function");
+}
 
+mod mod1 {
+    pub fn function () {
+        super::function();
+    }
+
+    pub mod mod2 {
+        fn function() {
+            println!("mod1::mod2::function");
+        }
+
+        pub fn call() {
+            super::function(); 
+        }
+    }
+}
 fn main() {
-    let data = stdfs::read("src/main.rs").unwrap();
-    println!("{}", String::from_utf8(data).unwrap());
+    mod1::function();
+    mod1::mod2::call();
 }
