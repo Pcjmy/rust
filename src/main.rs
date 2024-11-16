5187b28651d2bcfe4ec2a00841aa705f82a4019a
Pcjmy <1716045191@qq.com>
2024-11-16 15:40:49 +0800

    feat: 使用use绑定模块成员

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,6 @@
-mod mod1 {
-    pub struct Person {
-        pub name: String,
-        nickname: String,
-    }
-
-    impl Person {
-        pub fn new(name: &str) -> Self {
-            Person {
-                name: String::from(name),
-                nickname: String::new(),
-            }
-        }
-
-        pub fn set_nickname(&mut self, nickname: &str) {
-            self.nickname = String::from(nickname);
-        }
-
-        pub fn say_nickname(&self) {
-            println!("{}", self.nickname);
-        }
-    }
-}
+use std::fs as stdfs;
 
 fn main() {
-    let mut p = mod1::Person::new("jack");
-    println!("{}", p.name);
-    p.set_nickname("baby");
-    p.say_nickname();
+    let data = stdfs::read("src/main.rs").unwrap();
+    println!("{}", String::from_utf8(data).unwrap());
 }
