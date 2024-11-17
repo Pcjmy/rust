e7ff17a5ed8dc229e73850533d1b5ca9fc3ceff0
Pcjmy <1716045191@qq.com>
2024-11-17 18:36:50 +0800

    feat: 结构体泛型的实现

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@ struct Point<T> {
     y: T,
 }
 
-struct Line<T> {
-    x: Point<T>,
-    y: Point<T>,
+impl<T: Clone + std::cmp::PartialOrd> Point<T> {
+    fn largest(&self) -> T {
+        if self.x > self.y {
+            self.x.clone()
+        } else {
+            self.y.clone()
+        }
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
 
 fn main() {
-    let point1  = Point{ x: 0, y: 0 };
-    let point2  = Point{ x: 2, y: 2 };
-    let line = Line{ x: point1, y: point2 };
-    println!("{} {} {} {}", line.x.x, line.x.y, line.y.x, line.y.y);
+    let point = Point { x: 10, y: 20 };
+    println!("{:?}", point.largest());
+    let point1 = Point { x: 10.0, y: 20.0 };
+    println!("{:?}", point1.distance_from_origin());
 }
