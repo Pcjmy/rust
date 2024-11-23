734c333da93927b66b96487d24258e48c80ee447
Pcjmy <1716045191@qq.com>
2024-11-23 22:28:27 +0800

    feat: 自动派生

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
 }
 
 fn main() {
-    let p = Point { x: 10, y: 20 };
-    println!("{:?}", p);
+    let p1 = Point { x: 10, y: 20 };
+    let p2 = Point { x: 15, y: 20 };
+    println!("{}", p1 == p2);
 }
