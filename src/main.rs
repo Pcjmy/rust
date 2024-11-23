5c79fe4ce16d5e89c0984c62d6fd25d8edc30e57
Pcjmy <1716045191@qq.com>
2024-11-23 22:31:13 +0800

    feat: 自动派生

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 struct Point {
     x: i32,
     y: i32,
+    z: f64,
 }
 
 fn main() {
-    let p1 = Point { x: 10, y: 20 };
-    let p2 = Point { x: 15, y: 20 };
-    println!("{}", p1 == p2);
+    // let p1 = Point { x: 10, y: 20 };
+    // let p2 = Point { x: 15, y: 20 };
+    // println!("{}", p1 == p2);
 
     let p = Point::default();
     println!("{:?}", p);
