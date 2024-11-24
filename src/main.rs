a8415eb06fb680c7545f511da6acc8497e8a80cf
Pcjmy <1716045191@qq.com>
2024-11-24 18:57:16 +0800

    feat: 所有权

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-#[derive(Debug, PartialEq, Default)]
-struct Point {
-    x: i32,
-    y: i32,
-    z: f64,
-}
-
 fn main() {
-    // let p1 = Point { x: 10, y: 20 };
-    // let p2 = Point { x: 15, y: 20 };
-    // println!("{}", p1 == p2);
-
-    let p = Point::default();
-    println!("{:?}", p);
+    let s1 = String::from("Hello World");
+    let s2 = s1;
+    println!("{}", s2);
 }
