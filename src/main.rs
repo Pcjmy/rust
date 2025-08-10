b706547f088175fe9990a29ac55e55857cfc1c1e
Pcjmy <1716045191@qq.com>
2025-08-10 23:36:26 +0800

    feat: Box处理大数组分配

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ enum List {
 }
 
 fn main() {
-    let list = List::Cons(0, Box::new(List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))))));
+    let a = [0; 1024 * 512];
+    let a_box = Box::new(a);
 }
