65b3ca777ca484668fa169abfeb9ef48f827813d
Pcjmy <1716045191@qq.com>
2025-08-10 21:47:13 +0800

    feat: assert

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
     // panic! 宏
-    panic!("error!");
-    println!("here");
+    // panic!("error!");
+    // println!("here");
+
+    // 断言
+    // assert!(1 == 2);
+    assert_eq!(1, 2);
 }
