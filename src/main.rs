6a80896dc802972024f7c8db9b58db028812bd09
Pcjmy <1716045191@qq.com>
2025-08-10 21:51:50 +0800

    feat: unimplemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+fn add(a: u32, b: u32) -> u32 {
+    unimplemented!()
+}
+
 fn main() {
     // panic! 宏
     // panic!("error!");
@@ -5,5 +9,8 @@ fn main() {
 
     // 断言
     // assert!(1 == 2);
-    assert_eq!(1, 2);
+    // assert_eq!(1, 2);
+
+    // 未实现的代码 unimplemented!()
+    // let c = add(1, 2);
 }
