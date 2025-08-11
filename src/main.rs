492f3eef9abe8204b67e46892c682f08861a1c52
Pcjmy <1716045191@qq.com>
2025-08-12 00:28:12 +0800

    feat: Vector方法

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    for i in 0..10 {
-        v.push(i);
-    }
-    println!("{:?}", v[9]);
+    // let mut v: Vec<i32> = Vec::new();
+    // for i in 0..10 {
+    //     v.push(i);
+    // }
+
+    let v: Vec<i32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    
+    println!("{:?}", v.len());
+    println!("{:?}", v.capacity());
 }
