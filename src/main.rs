c38a2bdff55cbaf3a2f2083c0a6b58e9c38ebdc1
Pcjmy <1716045191@qq.com>
2025-08-12 00:23:33 +0800

    feat: Vector

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use std::rc::Rc;
-
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
-
 fn main() {
-    // 一个值可以有多个所有者
-    let four = Rc::new(List::Cons(4, Rc::new(List::Nil)));
-    let zero_one = List::Cons(0, Rc::new(List::Cons(1, four.clone())));
-    let two_three = List::Cons(2, Rc::new(List::Cons(3, four)));
+    let mut v: Vec<i32> = Vec::new();
+    for i in 0..10 {
+        v.push(i);
+    }
+    println!("{:?}", v[9]);
 }
