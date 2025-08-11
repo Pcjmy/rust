ba3c17e1ec74aec8b87243c5185b63653bfc7202
Pcjmy <1716045191@qq.com>
2025-08-12 00:40:20 +0800

    feat: Vector遍历

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,20 @@ fn main() {
     // }
 
     let mut v: Vec<i32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("{:?}", v.pop());
-    println!("{:?}", v.len());
-    println!("{:?}", v.capacity());
-    v.push(9);
-    v.push(10);
-    println!("{:?}", v.len());
-    println!("{:?}", v.capacity());
+    
+    // for i in 0..v.len() {
+    //     println!("v[{:?}]={:?}", i, v[i]);
+    // }
+
+    // for e in v.iter() {
+    //     println!("{:?}", e);
+    // }
+
+    for e in v.iter_mut() {
+        *e *= 2;
+    }
+
+    for i in 0..v.len() {
+        println!("v[{:?}]={:?}", i, v[i]);
+    }
 }
