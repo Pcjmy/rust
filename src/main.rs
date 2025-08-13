d8f5d164f5cde8e7a264f32b950b9b021a55321d
Pcjmy <1716045191@qq.com>
2025-08-13 23:54:07 +0800

    feat: 字符串

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
-use std::collections::HashMap;
+fn echo(s: &str) {
+    println!("{:?}", s);
+}
+
+struct Foo {
+    name: String,
+}
 
 fn main() {
-    let mut transcript: HashMap<&str, u32> = HashMap::new();
+    // "Hello World!" 字符串字面量存储在程序的数据段中
+    // "Hello World!" 叫做字符串的字面量
 
-    transcript.insert("alice", 95);
-    transcript.insert("bob", 92);
-    transcript.insert("jack", 97);
+    // str类型是底层的字符串切片类型，通常不会直接使用
+    // 在实际编程中，我们几乎总是使用 &str 字符串引用
+    // str 表示存储在内存中的实际字符串数据序列
+    // &str 可以引用数据段中的内容，也可以是堆里面的内容
+    let s: &'static str = "Hello World!";
 
-    // match transcript.get(&"alice") {
-    //     Some(data) => println!("alice's score is {:?}", data),
-    //     None => println!("alice is not in the transcript"),
-    // }
+    let mut t = String::from(s);
+    // String 类型是可变的字符串类型，拥有所有权
+    // 可以动态增长和修改其内容
+    // 数据存储在堆内存中，具有更灵活的内存管理能力
+    t.push_str("!!");
 
-    // match transcript.get(&"jack") {
-    //     Some(data) => println!("jack's score is {:?}", data),
-    //     None => println!("jack is not in the transcript"),
-    // }
+    println!("{:?}", t);
 
-    // transcript.remove(&"alice");
+    echo(s);
+    echo(&t);
 
-    // match transcript.get(&"alice") {
-    //     Some(data) => println!("alice's score is {:?}", data),
-    //     None => println!("alice is not in the transcript"),
-    // }
-
-    for (&name, &score) in transcript.iter() {
-        println!("{}'s score is {}", name, score);
-    }
+    let foo = Foo { name: String::from(s) };
+    println!("{:?}", foo.name);
 }
