87b57dbe4e8b7419cab91921ee659f8fe8dfdc28
Pcjmy <1716045191@qq.com>
2025-08-13 23:38:49 +0800

    feat: HashMap遍历

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,24 @@ fn main() {
 
     transcript.insert("alice", 95);
     transcript.insert("bob", 92);
+    transcript.insert("jack", 97);
 
-    match transcript.get(&"alice") {
-        Some(data) => println!("alice's score is {:?}", data),
-        None => println!("alice is not in the transcript"),
-    }
+    // match transcript.get(&"alice") {
+    //     Some(data) => println!("alice's score is {:?}", data),
+    //     None => println!("alice is not in the transcript"),
+    // }
 
-    match transcript.get(&"jack") {
-        Some(data) => println!("jack's score is {:?}", data),
-        None => println!("jack is not in the transcript"),
-    }
+    // match transcript.get(&"jack") {
+    //     Some(data) => println!("jack's score is {:?}", data),
+    //     None => println!("jack is not in the transcript"),
+    // }
 
-    transcript.remove(&"alice");
+    // transcript.remove(&"alice");
 
-    match transcript.get(&"alice") {
-        Some(data) => println!("alice's score is {:?}", data),
-        None => println!("alice is not in the transcript"),
-    }
+    // match transcript.get(&"alice") {
+    //     Some(data) => println!("alice's score is {:?}", data),
+    //     None => println!("alice is not in the transcript"),
+    // }
 
     for (&name, &score) in transcript.iter() {
         println!("{}'s score is {}", name, score);
