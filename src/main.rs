871f91bd5e27086c4dc0095fcc27a8e04a799b1a
Pcjmy <1716045191@qq.com>
2025-08-16 22:35:36 +0800

    feat: SystemTime

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
-fn echo(s: &str) {
-    println!("{:?}", s);
-}
-
-struct Foo {
-    name: String,
-}
+use std::time::{SystemTime};
 
 fn main() {
-    // "Hello World!" 字符串字面量存储在程序的数据段中
-    // "Hello World!" 叫做字符串的字面量
+    // SystemTime 是系统时间
+    // 通过系统调用请求操作系统返回的系统时间
+    let now = SystemTime::now();
+    println!("{:?}", now);
 
-    // str类型是底层的字符串切片类型，通常不会直接使用
-    // 在实际编程中，我们几乎总是使用 &str 字符串引用
-    // str 表示存储在内存中的实际字符串数据序列
-    // &str 可以引用数据段中的内容，也可以是堆里面的内容
-    let s: &'static str = "Hello World!";
-
-    let mut t = String::from(s);
-    // String 类型是可变的字符串类型，拥有所有权
-    // 可以动态增长和修改其内容
-    // 数据存储在堆内存中，具有更灵活的内存管理能力
-    t.push_str("!!");
-
-    println!("{:?}", t);
-
-    echo(s);
-    echo(&t);
-
-    let foo = Foo { name: String::from(s) };
-    println!("{:?}", foo.name);
+    let timestamp = now.duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    println!("timestamp = {:?}", timestamp);
 }
