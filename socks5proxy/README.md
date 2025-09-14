3a3544679e0daa03a2015c10f10dcb212db33091
Pcjmy <1716045191@qq.com>
2025-09-14 23:05:20 +0800

    feat: socks5proxy

--- socks5proxy/README.md
+++ socks5proxy/README.md
@@ -1,5 +1,16 @@
 ## README
 
+A simple SOCKS5 proxy server implemented in Rust.
+
+### Usage
+
 ```shell
 socks5proxy -l 127.0.0.1:8080
+curl --socks5-hostname 127.0.0.1:8080 https://www.baidu.com
 ```
+
+### Dependencies
+
+This project uses the following crates:
+- [argparse](https://github.com/tailhook/rust-argparse) - Command-line argument parsing
+- [byteorder](https://docs.rs/byteorder) - Library for reading/writing numbers in big-endian and little-endian formats
--- socks5proxy/src/main.rs
+++ socks5proxy/src/main.rs
@@ -1,5 +1,92 @@
+use std::io::Read;
+
+use byteorder::WriteBytesExt;
+
 fn hand(src_stream: &std::net::TcpStream) -> Result<(), Box<dyn std::error::Error>> {
     println!("src {}", src_stream.peer_addr().unwrap());
+
+    let mut src_reader = src_stream.try_clone()?;
+    let mut src_writer = src_stream.try_clone()?;
+
+    let mut buf: Vec<u8> = vec![0x00; 256];
+    src_reader.read_exact(&mut buf[0..1])?;
+    if buf[0] != 0x05 {
+        panic!("unreachable!");
+    }
+    src_reader.read_exact(&mut buf[0..1])?;
+    let nauth = buf[0] as usize;
+    src_reader.read_exact(&mut buf[0..nauth])?;
+
+    src_writer.write_u8(0x05)?;
+    src_writer.write_u8(0x00)?;
+
+    println!("greeting done");
+
+    src_reader.read_exact(&mut buf[0..1])?;
+    if buf[0] != 0x05 {
+        panic!("unreachable!");
+    }
+    src_reader.read_exact(&mut buf[0..1])?;
+    if buf[0] != 0x01 {
+        panic!("unreachable!");
+    }
+    src_reader.read_exact(&mut buf[0..1])?;
+    if buf[0] != 0x00 {
+        panic!("unreachable!");
+    }
+    src_reader.read_exact(&mut buf[0..1])?;
+    let host = match buf[0] {
+        0x01 => {
+            src_reader.read_exact(&mut buf[0..4])?;
+            std::net::Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]).to_string()
+        }
+        0x03 => {
+            src_reader.read_exact(&mut buf[0..1])?;
+            let l = buf[0] as usize;
+            src_reader.read_exact(&mut buf[0..l])?;
+            String::from_utf8_lossy(&buf[0..l]).to_string()
+        }
+        0x04 => {
+            src_reader.read_exact(&mut buf[0..16])?;
+            std::net::Ipv6Addr::new(
+                (buf[0x00] as u16) << 8 | (buf[0x01] as u16),
+                (buf[0x02] as u16) << 8 | (buf[0x03] as u16),
+                (buf[0x04] as u16) << 8 | (buf[0x05] as u16),
+                (buf[0x06] as u16) << 8 | (buf[0x07] as u16),
+                (buf[0x08] as u16) << 8 | (buf[0x09] as u16),
+                (buf[0x0a] as u16) << 8 | (buf[0x0b] as u16),
+                (buf[0x0c] as u16) << 8 | (buf[0x0d] as u16),
+                (buf[0x0e] as u16) << 8 | (buf[0x0f] as u16),
+            ).to_string()
+        }
+        _ => panic!("unreachable!")
+    };
+    src_reader.read_exact(&mut buf[0..2])?;
+    let port = (buf[0] as u16) << 8 | (buf[1] as u16);
+    let dst = format!("{}:{}", host, port);
+
+    println!("dst {}", dst);
+
+    let dst_stream = std::net::TcpStream::connect(&dst)?;
+    let mut dst_reader = dst_stream.try_clone()?;
+    let mut dst_writer = dst_stream.try_clone()?;
+
+    src_writer.write_u8(0x05)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x01)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x00)?;
+    src_writer.write_u8(0x00)?;
+
+    std::thread::spawn(move || {
+        std::io::copy(&mut src_reader, &mut dst_writer).ok();
+    });
+    std::io::copy(&mut dst_reader, &mut src_writer).ok();
+
     Ok(())
 }
 
